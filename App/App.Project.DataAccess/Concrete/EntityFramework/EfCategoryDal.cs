82f0a51a581daf019cb73c92f399cc84ca24a500
Ahmet KAYAR <ahmet.kayar@gmail.com>
2017-06-12 00:46:45 +0300

    Categori Kısmı Eklendi

--- App/App.Project.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
+++ App/App.Project.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -1,9 +1,6 @@
 ﻿using App.Core.Dal.EntityFramework;
 using App.Project.DataAccess.Abstract;
 using App.Project.Entites.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace App.Project.DataAccess.Concrete.EntityFramework
 {
